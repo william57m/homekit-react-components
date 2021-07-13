e04f36c93e2487776544709e92d81f2e6b00273e
Greg Hesp <greg.hesp@gmail.com>
2021-07-13 11:12:43 +0100

    Added Offline camera

--- src/components/CameraCard.js
+++ src/components/CameraCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
@@ -17,17 +17,51 @@ const CameraCardContainer = styled(CardContainer)`
   }
 `;
 
+const OfflineContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(${props => props.theme.cameraCard.height} - 30px);
+  color: white;
+  font-weight: bold;
+`;
+
 const Image = styled.img`
   width: 100%;
 `;
 
 export function CameraCard(props) {
+    const [loading, setLoading] = useState(true);
+    const [success, setSuccess] = useState(true);
+
+    useEffect(() => {
+        try {
+            fetch(props.imageSrc).then(r => {
+                if(r.status !== 200) {
+                    setSuccess(false);
+                }
+                setLoading(false);
+            });
+        } catch (e) {
+            setSuccess(false);
+            setLoading(false);
+        }
+    },[]);
+
   return (
     <CameraCardContainer>
       <CardTitle>{props.name}</CardTitle>
-      <Image src={props.imageSrc} />
+        {loading ? null : (success ? <Image src={props.imageSrc} /> : <Offline/>)}
     </CameraCardContainer>
   );
+}
+
+function Offline() {
+    return (
+        <OfflineContainer>
+            Camera Offline
+        </OfflineContainer>
+    );
 }
 
 CameraCard.propTypes = {
